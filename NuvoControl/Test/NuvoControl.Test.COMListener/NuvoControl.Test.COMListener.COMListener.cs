54f8a7d594646c8ed4e39ede5079846635bdca00
ImfeldC <imfeldc@gmail.com>
2009-05-10 11:47:56 +0000

    Erste zwei Test-Projkete für Serielle Schnittstelle (COM-Port) hinzugefügt.

---
+++ NuvoControl/Test/NuvoControl.Test.COMListener/NuvoControl.Test.COMListener.COMListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NuvoControl.Test.COMListener
+{
+    public partial class COMListener : Form
+    {
+        public COMListener()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            comManager = new CommunicationManager("9600", "None", "1", "8", "COM58", rtbCOM );
+            comManager.OpenPort();
+            comManager.WriteData("Hello..");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
