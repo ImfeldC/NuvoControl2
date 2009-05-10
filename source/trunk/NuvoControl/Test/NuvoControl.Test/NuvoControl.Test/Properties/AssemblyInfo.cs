6b1e26f5463dc2a52c8b4bc1920e9fca7636dbe7
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-10 16:50:28 +0000

    Add VSTS example.

--- source/trunk/NuvoControl/Test/NuvoControl.Test/NuvoControl.Test/Properties/AssemblyInfo.cs
+++ source/trunk/NuvoControl/Test/NuvoControl.Test/NuvoControl.Test/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuvoControl.Test.COMListener")]
+[assembly: AssemblyTitle("NuvoControl.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SEI")]
-[assembly: AssemblyProduct("NuvoControl.Test.COMListener")]
+[assembly: AssemblyProduct("NuvoControl.Test")]
 [assembly: AssemblyCopyright("Copyright © SEI 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1c1d88c3-9f0c-4374-9630-c0a73525276b")]
+[assembly: Guid("e0de9c0d-2ac6-4a68-89de-ed0c9bb518e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("NuvoControl.Test")]
