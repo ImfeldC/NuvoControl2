305a8734008d531a1631e28cfdb0153f44cee71f
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-16 13:26:08 +0000

    Arbeit vom Donnerstag, 14.Mai 2009 (Merged mit den Änderungen von Beni)

--- source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Test/Properties/AssemblyInfo.cs
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Test/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuvoControl.Server.ProtocolDriver")]
+[assembly: AssemblyTitle("NuvoControl.Server.ProtocolDriver.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Siemens and Partners")]
-[assembly: AssemblyProduct("NuvoControl.Server.ProtocolDriver")]
-[assembly: AssemblyCopyright("Copyright © Siemens and Partners 2009")]
+[assembly: AssemblyCompany("SEI")]
+[assembly: AssemblyProduct("NuvoControl.Server.ProtocolDriver.Test")]
+[assembly: AssemblyCopyright("Copyright © SEI 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("02b39ce0-f52e-4ad8-b8b1-b2fb76e872eb")]
+[assembly: Guid("f2e1d46d-5c9d-45e6-8315-67141c6ee31f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("NuvoControl.Server.ProtocolDriver.Test")]
---
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Test/SerialPortTest.cs
@@ -0,0 +1,139 @@
+﻿using NuvoControl.Server.ProtocolDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuvoControl.Server.ProtocolDriver.Interface;
+
+namespace NuvoControl.Server.ProtocolDriver.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for SerialPortTest and is intended
+    ///to contain all SerialPortTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class SerialPortTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Open
+        ///</summary>
+        [TestMethod()]
+        public void OpenTest()
+        {
+            SerialPort target = new SerialPort(); // TODO: Initialize to an appropriate value
+            SerialPortConnectInformation serialPortConnectInformation = null; // TODO: Initialize to an appropriate value
+            target.Open(serialPortConnectInformation);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for IsOpen
+        ///</summary>
+        [TestMethod()]
+        public void IsOpenTest()
+        {
+            SerialPort target = new SerialPort(); // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.IsOpen;
+            Assert.AreEqual(actual, false);
+        }
+
+        /// <summary>
+        ///A test for Write
+        ///</summary>
+        [TestMethod()]
+        public void WriteTest()
+        {
+            SerialPort target = new SerialPort();
+            string text = string.Empty; // TODO: Initialize to an appropriate value
+            target.Write(text);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+
+        /// <summary>
+        ///A test for OpenPort
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("NuvoControl.Server.ProtocolDriver.dll")]
+        public void OpenPortTest()
+        {
+            SerialPort_Accessor target = new SerialPort_Accessor(); // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.OpenPort();
+            Assert.AreEqual(false, actual);
+        }
+
+        /// <summary>
+        ///A test for Close
+        ///</summary>
+        [TestMethod()]
+        public void CloseTest()
+        {
+            SerialPort target = new SerialPort(); // TODO: Initialize to an appropriate value
+            SerialPortConnectInformation serialPortConnectInformation = null; // TODO: Initialize to an appropriate value
+            target.Open(serialPortConnectInformation);
+            target.Close();
+        }
+
+        /// <summary>
+        ///A test for SerialPort Constructor
+        ///</summary>
+        [TestMethod()]
+        public void SerialPortConstructorTest()
+        {
+            SerialPort target = new SerialPort();
+            bool actual = target.IsOpen;
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
