6ea200dd99ef87dbe5665e2102e4bc960d5d4470
ImfeldC <imfeldc@gmail.com>
2009-05-17 16:35:06 +0000

    Add ILog to the test project

--- NuvoControl/Test/NuvoControl.Test.COMListener/NuvoControl.Test.COMListener.Program.cs
+++ NuvoControl/Test/NuvoControl.Test.COMListener/NuvoControl.Test.COMListener.Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Common.Logging;
 
 namespace NuvoControl.Test.COMListener
 {
@@ -13,6 +14,9 @@ namespace NuvoControl.Test.COMListener
         [STAThread]
         static void Main()
         {
+            ILog log = LogManager.GetCurrentClassLogger();
+            log.Debug( m=>m("Start of COMListener! (Version={0})", Application.ProductVersion) );
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new COMListener());
