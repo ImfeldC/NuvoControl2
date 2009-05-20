1e50e37e3f4dfbb4d4fd79968d39df9065fc101a
ImfeldC <imfeldc@gmail.com>
2009-05-20 17:08:48 +0000

    Update, before move :-)

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEnumerations.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEnumerations.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 using System.Xml.Serialization;
 
 
-namespace NuvoControl.Common
+namespace NuvoControl.Server.ProtocolDriver
 {
 
 	//===========================================================
