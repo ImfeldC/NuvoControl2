a7f0506b43e4bd52522f74ef472c5f8d97d0d28d
ImfeldC <imfeldc@gmail.com>
2009-05-20 17:13:50 +0000

    Save work :-)

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/INuvoEssentiaProtocol.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/INuvoEssentiaProtocol.cs
@@ -5,7 +5,52 @@ using System.Text;
 
 namespace NuvoControl.Server.ProtocolDriver.Interface
 {
+    public delegate void NuvoEssentiaProtocolEventHandler(
+              object sender, NuvoEssentiaProtocolEventArgs e);
+
+    public class NuvoEssentiaProtocolEventArgs : EventArgs
+    {
+        string _msg;
+
+        public string Message
+        {
+            get { return _msg; }
+        }
+
+        public NuvoEssentiaProtocolEventArgs(string msg)
+        {
+            _msg = msg;
+        }
+
+    }
+
+    public class NuvoEssentiaCommand
+    {
+        ENuvoEssentiaCommands _command;
+
+        public NuvoEssentiaCommand(ENuvoEssentiaCommands command)
+        {
+            _command = command;
+        }
+
+        public NuvoEssentiaCommand(NuvoEssentiaProtocolEventArgs args)
+        {
+            _command = 0;
+        }
+    }
+
     interface INuvoEssentiaProtocol
     {
+
+        event NuvoEssentiaProtocolEventHandler onCommandReceived;
+
+        void Open(SerialPortConnectInformation serialPortConnectInformation);
+
+        void Close();
+
+        void SendCommand(string command);
+
+        void SendCommand(NuvoEssentiaCommand command);
+    
     }
 }
