05a3b2702b219da5113b208b075693e3f40a3da2
ImfeldC <imfeldc@gmail.com>
2009-05-27 19:58:25 +0000

    Add first version of IProtocol

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/IProtocol.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/IProtocol.cs
@@ -5,8 +5,44 @@ using System.Text;
 
 namespace NuvoControl.Server.ProtocolDriver.Interface
 {
+    public delegate void ProtocolEventHandler(
+              object sender, ProtocolEventArgs e);
+
+    public class ProtocolEventArgs : EventArgs
+    {
+        ICommand _command;
+
+        public ICommand Command
+        {
+            get { return _command; }
+        }
+
+        public ProtocolEventArgs(ICommand command)
+        {
+            _command = command;
+        }
+
+    }
+
+    public interface ICommand : IComparable
+    {
+        Guid Guid { get; }
+
+        DateTime CreatedDateTime { get; }
+        DateTime SendDateTime { get; }
+        DateTime ReceiveDateTime { get; }
+
+        ENuvoEssentiaZones ZoneId { get; }
+        ENuvoEssentiaSources SourceId { get; }
+        EZonePowerStatus PowerStatus { get; }
+        int VolumeLevel { get; }
+    }
+
 
     public interface IProtocol
     {
+        event ProtocolEventHandler onCommandReceived;
+
+        void SendCommand(ICommand command);
     }
 }
