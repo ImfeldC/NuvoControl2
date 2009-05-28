b65f86106509f71e537d311a239a382db2ef039a
ImfeldC <imfeldc@gmail.com>
2009-05-28 21:46:04 +0000

    Add batch file to start unit tests

---
+++ NuvoControl/RunTests.bat
@@ -0,0 +1,2 @@
+del TestResults.xml
+"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\MSTest.exe" /testcontainer:NuvoControl\Server\NuvoControl.Server.ProtocolDriver.UnitTest\bin\Debug\NuvoControl.Server.ProtocolDriver.Test.dll /resultsfile:TestResults.xml
