cbeca4b7c28e9b0a155c330881ed881a121118f5
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-28 22:04:28 +0000

    Force build ...

--- source/trunk/RunTests.bat
+++ source/trunk/RunTests.bat
@@ -5,3 +5,4 @@ del TestResults2.xml
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\MSTest.exe" /testcontainer:NuvoControl\Server\NuvoControl.Server.Service.UnitTest\bin\Debug\NuvoControl.Server.Service.UnitTest.dll /resultsfile:TestResults2.xml
 del TestResults3.xml
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\MSTest.exe" /testcontainer:NuvoControl\Server\NuvoControl.Server.Dal.UnitTest\bin\Debug\NuvoControl.Server.Dal.UnitTest.dll /resultsfile:TestResults3.xml
+rem Finish Unit Test of Visual Studio 2008 ...
