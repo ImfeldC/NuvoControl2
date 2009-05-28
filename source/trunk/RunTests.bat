62a14dea58523a90ebdbb48e1a545d2179ac0500
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-05-28 21:53:34 +0000

    Force build ...

--- source/trunk/RunTests.bat
+++ source/trunk/RunTests.bat
@@ -1,2 +1,3 @@
+rem Start Unit Test of Visual Studio 2008 ...
 del TestResults.xml
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\MSTest.exe" /testcontainer:NuvoControl\Server\NuvoControl.Server.ProtocolDriver.UnitTest\bin\Debug\NuvoControl.Server.ProtocolDriver.Test.dll /resultsfile:TestResults.xml
