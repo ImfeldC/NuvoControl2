97f0fd84ff3428ba871d8aed7786a4d5826a686e
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-06-07 16:56:50 +0000

    Add COMListener as base for the Nuvo Client

---
+++ source/trunk/NuvoControl/Test/NuvoControl.Test.NuvoClient/NuvoControl.Test.COMListener.Program.cs
@@ -0,0 +1,57 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	NuvoControl.Test.COMListener\NuvoControl.Test.COMListener.Program.cs
+///
+/// \brief	Implements the NuvoControl.Test.COMListener.Program class. 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Common.Logging;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \namespace	NuvoControl.Test.COMListener
+///
+/// \brief	Contains test client "COMListener". 
+/// 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace NuvoControl.Test.COMListener
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// \class	Program
+    ///
+    /// \brief	Program. Main program class of COMListener
+    ///
+    /// \author	Administrator
+    /// \date	17.05.2009
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    static class Program
+    {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// \fn	[STAThread] static void Main()
+        ///
+        /// \brief	
+        /// 		The main entry point for the application. 
+        /// 		Uses the ILog intercface for common logging.
+        ///
+        /// \author	Administrator
+        /// \date	17.05.2009
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [STAThread]
+        static void Main()
+        {
+            ILog log = LogManager.GetCurrentClassLogger();
+            log.Debug( m=>m("Start of COMListener! (Version={0})", Application.ProductVersion) );
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new COMListener());
+
+            log.Debug(m => m("End of COMListener! (Version={0})", Application.ProductVersion));
+        }
+    }
+}
