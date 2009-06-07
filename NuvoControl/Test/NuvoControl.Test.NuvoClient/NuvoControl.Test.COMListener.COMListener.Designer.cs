9022e47d156a38406713b2bef79bb9d372c27718
ImfeldC <imfeldc@gmail.com>
2009-06-07 16:56:50 +0000

    Add COMListener as base for the Nuvo Client

---
+++ NuvoControl/Test/NuvoControl.Test.NuvoClient/NuvoControl.Test.COMListener.COMListener.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace NuvoControl.Test.COMListener
+{
+    partial class COMListener
+    {
+
+        CommunicationManager comManager;
+
+
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbCOM = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.cmbComSelect = new System.Windows.Forms.ComboBox();
+            this.txtboxSendText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbCOM
+            // 
+            this.rtbCOM.Location = new System.Drawing.Point(13, 48);
+            this.rtbCOM.Name = "rtbCOM";
+            this.rtbCOM.Size = new System.Drawing.Size(535, 213);
+            this.rtbCOM.TabIndex = 0;
+            this.rtbCOM.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(108, 14);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(473, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbComSelect
+            // 
+            this.cmbComSelect.FormattingEnabled = true;
+            this.cmbComSelect.Location = new System.Drawing.Point(13, 14);
+            this.cmbComSelect.Name = "cmbComSelect";
+            this.cmbComSelect.Size = new System.Drawing.Size(89, 21);
+            this.cmbComSelect.Sorted = true;
+            this.cmbComSelect.TabIndex = 10;
+            // 
+            // txtboxSendText
+            // 
+            this.txtboxSendText.Location = new System.Drawing.Point(206, 16);
+            this.txtboxSendText.Name = "txtboxSendText";
+            this.txtboxSendText.Size = new System.Drawing.Size(261, 20);
+            this.txtboxSendText.TabIndex = 11;
+            this.txtboxSendText.Text = "Hello...";
+            // 
+            // COMListener
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 273);
+            this.Controls.Add(this.txtboxSendText);
+            this.Controls.Add(this.cmbComSelect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.rtbCOM);
+            this.Name = "COMListener";
+            this.Text = "NuvoControl.Test.COMListener";
+            this.Load += new System.EventHandler(this.COMListener_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox rtbCOM;
+        private Button btnConnect;
+        private Button btnSend;
+        private ComboBox cmbComSelect;
+        private TextBox txtboxSendText;
+
+    }
+}
+
---
+++ NuvoControl/Test/NuvoControl.Test.NuvoClient/NuvoControl.Test.COMListener.COMListener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace NuvoControl.Test.COMListener
+{
+    public partial class COMListener : Form
+    {
+        public COMListener()
+        {
+            InitializeComponent();
+
+            // Nice methods to browse all available ports:
+            string[] ports = SerialPort.GetPortNames();
+
+            // Add all port names to the combo box:
+            foreach (string port in ports)
+            {
+                cmbComSelect.Items.Add(port);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            comManager = new CommunicationManager("9600", "None", "1", "8", cmbComSelect.Text, rtbCOM);
+            comManager.OpenPort();
+            comManager.WriteData("Hello..");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comManager.WriteData(txtboxSendText.Text);
+        }
+
+        private void COMListener_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
