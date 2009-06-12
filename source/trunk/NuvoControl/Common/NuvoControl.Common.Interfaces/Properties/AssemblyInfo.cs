3c3ab0797e85bf321ff11f9bdf8ed1825630c067
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-06-12 22:02:07 +0000

    Make Assembly Information common for the whole solution. See examples on http://blogs.msdn.com/jjameson/archive/2009/04/03/shared-assembly-info-in-visual-studio-projects.aspx http://weblogs.asp.net/savanness/archive/2003/07/22/10417.aspx

--- source/trunk/NuvoControl/Common/NuvoControl.Common.Interfaces/Properties/AssemblyInfo.cs
+++ source/trunk/NuvoControl/Common/NuvoControl.Common.Interfaces/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  21.05.2009
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) 21.05.2009, Bernhard Limacher: Initial implementation.
+ * 
+ **************************************************************************************************/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,31 +24,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NuvoControl.Common.Interfaces")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Siemens and Partners")]
-[assembly: AssemblyProduct("NuvoControl.Common.Interfaces")]
-[assembly: AssemblyCopyright("Copyright © Siemens and Partners 2009")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("09e90e61-b901-44ef-8650-1b6daa6f1187")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
