190311ca0a954a0a32b1e618bea82744000c8c49
ImfeldC <imfeldc@gmail.com>
2009-06-12 22:02:07 +0000

    Make Assembly Information common for the whole solution. See examples on http://blogs.msdn.com/jjameson/archive/2009/04/03/shared-assembly-info-in-visual-studio-projects.aspx http://weblogs.asp.net/savanness/archive/2003/07/22/10417.aspx

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/Properties/AssemblyInfo.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver.Interface/Properties/AssemblyInfo.cs
@@ -25,31 +25,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NuvoControl.Server.ProtocolDriver.Interface")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SEI")]
-[assembly: AssemblyProduct("NuvoControl.Server.ProtocolDriver.Interface")]
-[assembly: AssemblyCopyright("Copyright © SEI 2009")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("588c08cc-d6fd-4fff-b092-bd9feadbd301")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver.UnitTest/Properties/AssemblyInfo.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver.UnitTest/Properties/AssemblyInfo.cs
@@ -25,30 +25,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NuvoControl.Server.ProtocolDriver.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SEI")]
-[assembly: AssemblyProduct("NuvoControl.Server.ProtocolDriver.Test")]
-[assembly: AssemblyCopyright("Copyright © SEI 2009")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f2e1d46d-5c9d-45e6-8315-67141c6ee31f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
