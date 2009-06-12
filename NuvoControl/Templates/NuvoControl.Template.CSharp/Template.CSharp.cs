8ec9daf4772eed79c1c64e8df837d69024b24311
ImfeldC <imfeldc@gmail.com>
2009-06-12 20:45:16 +0000

    Add C# templates for NuvoControl project

---
+++ NuvoControl/Templates/NuvoControl.Template.CSharp/Template.CSharp.cs
@@ -0,0 +1,62 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) $year$ by B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     $rootnamespace$
+ *   Author:         $username$
+ *   Creation Date:  $time$
+ *   File Name:      $safeitemname$.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * Revisions:
+ * 1) $time$, $username$: Initial implementation.
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Common.Logging;
+
+
+
+namespace $rootnamespace$
+{
+
+    public class $safeitemname$
+    {
+        #region Fields
+
+        private static ILog _log = LogManager.GetCurrentClassLogger();
+
+
+        #endregion
+
+
+        #region Constructors
+        public $safeitemname$()
+        {
+        }
+        #endregion
+
+
+        #region Field Accessors
+
+        #endregion
+    }
+
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) $year$ by B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
+
+
