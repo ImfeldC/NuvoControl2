d718916e239112eba024071d2dece34bebdcf473
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-06-15 19:38:47 +0000

    - Split IProtocol and INuvoProtocol - Add method SetZoneState to IProtocol - Add Namespace documentation

---
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NamespaceDoc.cs
@@ -0,0 +1,39 @@
+﻿//
+// This file contains only namepsace documentation.
+// 
+// The <summary> tag is not supported in Visual Studio
+// for namespaces, because of that we use a feature of
+// Sandcastle to document namespaces. A class with the name
+// NamespaceDoc can be used to document a namespace.
+// 
+
+namespace NuvoControl.Server.ProtocolDriver
+{
+    /// <summary>
+    /// This namespace is used to hold all classes and interfaces related to the protocol driver stack.
+    /// <br/>The following class diagram shows the protocol stack: <a href="../ClassDiagrams/ProtocolDriverOverview.jpg">ProtocolDriverOverview</a>
+    /// </summary>
+    #region Namespace Documentation (required for Sandcastle documentation tool)
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+    #endregion
+}
+
+namespace NuvoControl.Server.ProtocolDriver.Simulator
+{
+    /// <summary>
+    /// This namespace contains classes for the in-build simulator of the protocol stack.
+    /// This in-build simulation mode is always available, it doesn't require external simulation
+    /// processes, etc. But it doens't support the full range of simulation modes. For this case
+    /// you should use the external available simulator.
+    /// <br/>The following class diagram shows the protocol stack: <a href="../ClassDiagrams/ProtocolDriverOverview.jpg">ProtocolDriverOverview</a>
+    /// </summary>
+    #region Namespace Documentation (required for Sandcastle documentation tool)
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+    #endregion
+}
