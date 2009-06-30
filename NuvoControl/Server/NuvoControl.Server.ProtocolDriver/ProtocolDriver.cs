1ea719b0628e42635d3e129fb93481891bc6ceb8
ImfeldC <imfeldc@gmail.com>
2009-06-30 05:56:43 +0000

    Improve generated html output. I suggest to use doxygen, instead of SandCastle (NDoc) (see "NuvoControl.Server.ProtocolDriver.NuvoEssentiaProtocolDriver Class Reference")

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver/ProtocolDriver.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver/ProtocolDriver.cs
@@ -37,8 +37,13 @@ namespace NuvoControl.Server.ProtocolDriver
     /// <c>IProtocol</c> interface.
     /// An application should use this class to instantiate a protocol stack to one or more
     /// devices.
-    /// <br/>The following class diagram shows the interface hirarchy: <a href="../ClassDiagrams/ProtocolDriverInterfaceOverview.jpg">ProtocolDriverInterfaceOverview</a>
-    /// <br/>The following class diagram shows the protocol stack: <a href="../ClassDiagrams/ProtocolDriverOverview.jpg">ProtocolDriverOverview</a>
+    /// 
+    /// <br/>The following class diagram shows the interface hirarchy: 
+    /// <a href="../ClassDiagrams/ProtocolDriverInterfaceOverview.jpg">ProtocolDriverInterfaceOverview</a>
+    /// \image html ProtocolDriverInterfaceOverview.jpg
+    /// <br/>The following class diagram shows the protocol stack: 
+    /// <a href="../ClassDiagrams/ProtocolDriverOverview.jpg">ProtocolDriverOverview</a>
+    /// \image html ProtocolDriverOverview.jpg
     /// </summary>
     public class NuvoEssentiaProtocolDriver : INuvoProtocol, IDisposable
     {
