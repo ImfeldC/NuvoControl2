4d298422164b9965da346e832f0668688fbb1261
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-08-04 19:21:11 +0000

    Optimize output. Still not ok, a warning is generated by Visual Studio, because of the <BR> html tag.

--- source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEssentiaSingleCommand.cs
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/NuvoEssentiaSingleCommand.cs
@@ -41,21 +41,21 @@ namespace NuvoControl.Server.ProtocolDriver
     /// This class contains public and private methods to replace placeholders
     /// in the command string.
     /// 
-    /// The following placeholders are known, either in incoming (=I) and/or outgoing (=O) commands:
-    /// I O 
-    /// x -  aa,bb,cc,dd,ee,ff --> for the IR carrier frequency. Replaced by replacePlaceholderForIRFrequency().
-    /// x x  xx  --> Zone Id. Replaced by replacePlaceholderForZone(). Parsed with parseCommandForZone().
-    /// x -  ppp --> Zone power status. Replaced by replacePlaceholderForPowerStatus(). Parsed with parseCommandForPowerStatus().
-    /// x x  s   --> Source id. Replaced by replacePlaceholderForSource(). Parsed with parseCommandForSource().
-    /// x -  q   --> Source group (on/off). Parsed with parseCommandForSourceGroupStatus().
-    /// x x  yy  --> Volume level. Replaced by replacePlaceholderWithVolumeLevel(). Parsed with parseCommandForVolumeLevel().
-    /// x -  i   --> DIP switch overridden (on/off). Parsed with parseCommandForDIPSwitchOverrideStatus().
-    /// x x  uuu  --> Bass EQ level. Replaced by replacePlaceholderWithBassTrebleLevel(). Parsed with parseCommandForBassLevel().
-    /// x x  ttt  --> Treble EQ level. Replaced by replacePlaceholderWithBassTrebleLevel(). Parsed with parseCommandForTrebleLevel().
-    /// x -  r   --> Volume reset (on/off). Parsed with parseCommandForVolumeResetStatus().
-    /// x -  vz.zz --> Firmware version. Parsed with parseCommandForFirmwareVersion().
-    /// 
-    /// In outgoing commands Replacement methods are used. 
+    /// The following placeholders are known, either in incoming (=I) and/or outgoing (=O) commands:<BR>
+    /// I O <BR>
+    /// x -  aa,bb,cc,dd,ee,ff --> for the IR carrier frequency. Replaced by replacePlaceholderForIRFrequency().<BR>
+    /// x x  xx  --> Zone Id. Replaced by replacePlaceholderForZone(). Parsed with parseCommandForZone().<BR>
+    /// x -  ppp --> Zone power status. Replaced by replacePlaceholderForPowerStatus(). Parsed with parseCommandForPowerStatus().<BR>
+    /// x x  s   --> Source id. Replaced by replacePlaceholderForSource(). Parsed with parseCommandForSource().<BR>
+    /// x -  q   --> Source group (on/off). Parsed with parseCommandForSourceGroupStatus().<BR>
+    /// x x  yy  --> Volume level. Replaced by replacePlaceholderWithVolumeLevel(). Parsed with parseCommandForVolumeLevel().<BR>
+    /// x -  i   --> DIP switch overridden (on/off). Parsed with parseCommandForDIPSwitchOverrideStatus().<BR>
+    /// x x  uuu  --> Bass EQ level. Replaced by replacePlaceholderWithBassTrebleLevel(). Parsed with parseCommandForBassLevel().<BR>
+    /// x x  ttt  --> Treble EQ level. Replaced by replacePlaceholderWithBassTrebleLevel(). Parsed with parseCommandForTrebleLevel().<BR>
+    /// x -  r   --> Volume reset (on/off). Parsed with parseCommandForVolumeResetStatus().<BR>
+    /// x -  vz.zz --> Firmware version. Parsed with parseCommandForFirmwareVersion().<BR>
+    /// <BR>
+    /// In outgoing commands Replacement methods are used.
     /// For the incoming commands refer the Parse Section.
     /// 
     /// </summary>
