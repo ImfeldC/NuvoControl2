78d3a13ad5873f81608f4c4776555cfc1dc4165f
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-08-13 21:30:05 +0000

    Re-enable the ping interval. Set it per default to 10[s]

--- source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/Properties/Settings.Designer.cs
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.ProtocolDriver/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace NuvoControl.Server.ProtocolDriver.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public ulong PingIntervall {
             get {
                 return ((ulong)(this["PingIntervall"]));
