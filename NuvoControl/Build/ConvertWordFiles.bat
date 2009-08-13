967b948e732fa6d0ed8ea54abd1652ac5295b68c
ImfeldC <imfeldc@gmail.com>
2009-08-13 07:57:36 +0000

    Setup scripts to convert word documents into html

---
+++ NuvoControl/Build/ConvertWordFiles.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Start converting word file ....
+cd E:\NuvoControl_Documentation\UserDocumentation
+cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs UserDocumentation NuvoControl_8100_ProjektKurzbeschreibung.doc 
