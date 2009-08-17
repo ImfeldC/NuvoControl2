a1754c63ab324c6b66233dffeb1e818ca92acf55
ImfeldC <imfeldc@gmail.com>
2009-08-17 21:55:45 +0000

    - adjust path

--- NuvoControl/Build/ConvertWordFiles.bat
+++ NuvoControl/Build/ConvertWordFiles.bat
@@ -1,20 +1,13 @@
 @echo off
-echo Cleanup ..
-e:
-cd E:\doxygen\
-rmdir /s /q Documentation
-echo Create directory ...
-cd E:\doxygen\
-mkdir Documentation
 echo Start converting word file ....
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs UserDocumentation NuvoControl_8100_ProjektKurzbeschreibung.doc 
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs UserDocumentation NuvoControl_8600_SetupGuideNuvoControl.docx
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs Vision   NuvoControl_0110_Projektantrag.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs Analyse  NuvoControl_1202_Anforderungsspezifikation.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs Design   NuvoControl_1300_Software_Architektur_und_Design.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0010_StundenNachweis.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0040_FeatureList_TaskList.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0100_ProjektHandbuch.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0103_Glossary.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0105_DocumentStatusList.doc
-cscript E:\NuvoControl_Trunk\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0001_Pendenzenliste.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs UserDocumentation NuvoControl_8100_ProjektKurzbeschreibung.doc 
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs UserDocumentation NuvoControl_8600_SetupGuideNuvoControl.docx
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs Vision   NuvoControl_0110_Projektantrag.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs Analyse  NuvoControl_1202_Anforderungsspezifikation.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs Design   NuvoControl_1300_Software_Architektur_und_Design.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0010_StundenNachweis.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0040_FeatureList_TaskList.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0100_ProjektHandbuch.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0103_Glossary.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0105_DocumentStatusList.doc
+cscript E:\ccnet\NuvoControl\NuvoControl\Build\ConvertDoc2html.vbs ProjektManagement NuvoControl_0001_Pendenzenliste.doc
