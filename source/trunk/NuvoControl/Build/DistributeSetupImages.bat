91d54c3b475a8befb4d03789092186531a6bc0df
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-08-18 11:06:21 +0000

    Use correct path :-(

--- source/trunk/NuvoControl/Build/DistributeSetupImages.bat
+++ source/trunk/NuvoControl/Build/DistributeSetupImages.bat
@@ -1,8 +1,9 @@
 @echo off
+echo Distribute Setup images (DistributeSetupImages.bat)
 rmdir /s /q E:\doxygen\html\setup
 mkdir E:\doxygen\html\setup
 mkdir E:\doxygen\html\setup\debug\
 mkdir E:\doxygen\html\setup\release\
-copy E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\debug .\debug\
-copy E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\release .\release\
+copy /Y E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\debug E:\doxygen\html\setup\debug\
+copy /Y E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\release E:\doxygen\html\setup\release\
 echo Done ...
