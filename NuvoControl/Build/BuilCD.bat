249d35406325e9c337df7d2b724fbee6277454dc
ImfeldC <imfeldc@gmail.com>
2009-08-23 08:35:18 +0000

    Add username and password for svn export ...

--- NuvoControl/Build/BuilCD.bat
+++ NuvoControl/Build/BuilCD.bat
@@ -3,16 +3,21 @@ echo Remove previous image e:\NuvoControl ...
 rmdir /S /Q e:\NuvoControl
 echo Create destination directory
 mkdir e:\NuvoControl
+
 echo copy PDF files ...
 mkdir e:\NuvoControl\pdf\
 robocopy  e:\doxygen\pdf\ e:\NuvoControl\pdf\ /E
+
 echo copy HTML files ...
 mkdir e:\NuvoControl\html\
 robocopy  e:\doxygen\html\ e:\NuvoControl\html\ /E
+
 echo export source files ...
-svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/source/trunk e:\NuvoControl\source\
+svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/source/trunk e:\NuvoControl\source\  --username NuvoControlGuest --password NuvoControlGuest
+
 echo export documentation files ...
-svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/documentation e:\NuvoControl\documentation\
+svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/documentation e:\NuvoControl\documentation\  --username NuvoControlGuest --password NuvoControlGuest
+
 echo export setup image (release) ...
 mkdir e:\NuvoControl\setup\
 robocopy E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\Release\ e:\NuvoControl\setup\ /E
