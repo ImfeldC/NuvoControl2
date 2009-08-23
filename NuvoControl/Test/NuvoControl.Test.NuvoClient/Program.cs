b67b0f618f932ff05e0cc351c0fc41cc56ca340b
ImfeldC <imfeldc@gmail.com>
2009-08-23 16:19:53 +0000

    Add description of the NuvoControl tools

--- NuvoControl/Test/NuvoControl.Test.NuvoClient/Program.cs
+++ NuvoControl/Test/NuvoControl.Test.NuvoClient/Program.cs
@@ -16,6 +16,57 @@
  * 
  **************************************************************************************************/
 
+/*!
+
+ \page pageTool_NuvoClient NuvoClient
+
+ This tool is a helper tool, used in case of debug and maintance tasks.
+ 
+ It is \b not used for the regular system runtime.
+ 
+ This tool is able to connect to several serial port types, like regular COM port, the MSMQ system and
+ in-built simulator object.
+ It allows to send and/or receive data to/from the connected end-point.
+ 
+ \section secToolNC_StartConnection Description: Start a connection
+ 
+ With the drop-down box at the top the required serial port device can be selected.
+ At start-up the tool reads all available serial COM ports on the machine and adds them to the list 
+ (e.g. COM1, COM2, etc.).
+ 
+ In addition it adds also the two queues used by NuvoControl to communicate between the simulator
+ and the NuvoControl server. You can either select a queue directly, or with QUEUE you can select 
+ the serial port class implementing the queue access (see \ref SerialPortQueue ).
+ 
+ Beside that it allows also to connect to the in-built simulator class (see \ref ProtocolDriverSimulator )
+ 
+ In the future you can easely add any other class which implements the ISerialPort interface.
+
+ With the checkbox 'Receive' you can enable/disable incoming messages.
+ 
+ With the checkbox 'Send' you can enable/disobale outgoing messages.
+ 
+ To connect press the 'Connect' button.
+ 
+ The lower part conatins a text area, where all incoming and outgoing data is traced.
+ 
+ 
+ \section secToolNC_SendAnyText Description: Send any text
+ 
+ The area 'Send Text' allows to send any text you like to the connected end-point.
+ 
+ Type your message in the text field and press 'Send' to send the content of the text field.
+ 
+ \section secToolNC_SendCommand Description: Send a Nuvo  command
+
+ The second area 'Send Nuvo Command' allows to send a Nuvo command to the connected end-point.
+ 
+ Select the data like Zone, Source, etc. and press 'Send' to send the corresponding command.
+
+ <BR>
+ Back to \ref main "main page"
+ 
+*/
 
 using System;
 using System.Collections.Generic;
--- NuvoControl/Test/NuvoControl.Test.RS232/NuvoControl.Test.RS232.Program.cs
+++ NuvoControl/Test/NuvoControl.Test.RS232/NuvoControl.Test.RS232.Program.cs
@@ -19,6 +19,36 @@
 // http://www.codeproject.com/KB/system/rs232ThreadSafe.aspx
 //
 
+/*!
+
+ \page pageTool_RS232Tester RS232 Tester
+
+ This tool is a helper tool, used in case of debug and maintance tasks.
+ 
+ It is \b not used for the regular system runtime.
+ 
+ This tool is able to connect to a regular serial COM port.
+ It allows to send data to the connected end-point, and to receive its answer.
+ 
+ With the drop-down box at the top the required serial port device can be selected.
+ At start-up the tool reads all available serial COM ports on the machine and adds them to the list 
+ (e.g. COM1, COM2, etc.).
+ 
+ As soon the COM port has been selected, a connection is established. You do not need to press any additional button.
+ 
+ To send any text, enter the text in the text field and press the 'Send' button. 
+
+ Any incoming data is automatically written to the text box. 
+
+ <BR>
+ Back to \ref main "main page"
+ 
+*/
+
+
+
+
+
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
