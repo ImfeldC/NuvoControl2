49e774eb7a307bb27ca57d96a769b8c11d55fca0
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-09-02 18:24:30 +0000

    Add Function Factory ....

---
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.FunctionServer/ConcreteFunctionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Common.Configuration;
+
+namespace NuvoControl.Server.FunctionServer
+{
+    class ConcreteFunctionFactory
+    {
+        public static IConcreteFunction instantiateConcreteFuntion(Function function)
+        {
+            if (typeof(SleepFunction) == function.GetType())
+            {
+                return new ConcreteSleepFunction((SleepFunction)function);
+            }
+            else if(typeof(AlarmFunction) == function.GetType())
+            {
+                return new ConcreteAlarmFunction((AlarmFunction)function);
+            }
+            return null;
+        }
+    }
+}
