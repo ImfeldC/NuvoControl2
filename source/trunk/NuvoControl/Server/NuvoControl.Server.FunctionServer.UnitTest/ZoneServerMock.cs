600c0f9652e5932b70097ecea11b11757b4fea59
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-09-03 20:25:32 +0000

    Add unit test for sleep function ...

---
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.FunctionServer.UnitTest/ZoneServerMock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuvoControl.Server.ZoneServer;
+using NuvoControl.Common.Configuration;
+using NuvoControl.Common;
+
+namespace NuvoControl.Server.FunctionServer.UnitTest
+{
+    class ZoneServerMock : IZoneServer
+    {
+        public bool _started = false;
+        public Dictionary<Address, ZoneState> _zoneStates = new Dictionary<Address, ZoneState>();
+        public List<Address> _monitoredZones = new List<Address>();
+
+        #region IZoneServer Members
+
+        public void StartUp()
+        {
+            _started = true;
+        }
+
+        public ZoneState GetZoneState(Address zoneId)
+        {
+            return _zoneStates[zoneId];
+        }
+
+        public void SetZoneState(Address zoneId, ZoneState zoneState)
+        {
+            _zoneStates.Add(zoneId, zoneState);
+        }
+
+        public void Monitor(Address zoneId, ZoneNotification subscriber)
+        {
+            _monitoredZones.Add(zoneId);
+        }
+
+        public void RemoveMonitor(Address zoneId, ZoneNotification subscriber)
+        {
+            _monitoredZones.Remove(zoneId);
+        }
+
+        #endregion
+    }
+}
