521f480eabddad065b707ebf7eae83a1c0f5ad9d
ImfeldC <imfeldc@gmail.com>
2009-09-04 16:46:15 +0000

    - don't distribute source code - don't distribute documentation - don't add own PDF folder anymore

--- NuvoControl/Build/BuilCD.bat
+++ NuvoControl/Build/BuilCD.bat
@@ -9,19 +9,9 @@ rmdir /S /Q e:\NuvoControl
 echo Create destination directory
 mkdir e:\NuvoControl
 
-echo copy PDF files ...
-mkdir e:\NuvoControl\pdf\
-robocopy  e:\doxygen\pdf\ e:\NuvoControl\pdf\ /E
-
 echo copy HTML files ...
 mkdir e:\NuvoControl\html\
 robocopy  e:\doxygen\html\ e:\NuvoControl\html\ /E
-
-echo export source files ...
-svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/source/trunk e:\NuvoControl\source\  --username NuvoControlGuest --password NuvoControlGuest
-
-echo export documentation files ...
-svn export https://svn2.xp-dev.com/svn/ImfeldC-NuvoControl/documentation e:\NuvoControl\documentation\  --username NuvoControlGuest --password NuvoControlGuest
 
 echo export setup image (release) ...
 mkdir e:\NuvoControl\setup\
