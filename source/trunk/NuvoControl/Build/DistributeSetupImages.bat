2c1b953ff1c6a918280964391b12235167bbdb55
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2009-09-04 19:16:12 +0000

    Add Imfi Config files ...

--- source/trunk/NuvoControl/Build/DistributeSetupImages.bat
+++ source/trunk/NuvoControl/Build/DistributeSetupImages.bat
@@ -4,6 +4,8 @@ rmdir /s /q E:\doxygen\html\setup
 mkdir E:\doxygen\html\setup
 mkdir E:\doxygen\html\setup\debug\
 mkdir E:\doxygen\html\setup\release\
+mkdir E:\doxygen\html\setup\config\
 copy /Y E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\debug E:\doxygen\html\setup\debug\
 copy /Y E:\ccnet\NuvoControl\NuvoControl\NuvoControlSetup\release E:\doxygen\html\setup\release\
+copy /Y E:\ccnet\NuvoControl\NuvoControl\Config\Eichholzweg9E.zip E:\doxygen\html\setup\config\
 echo Done ...
