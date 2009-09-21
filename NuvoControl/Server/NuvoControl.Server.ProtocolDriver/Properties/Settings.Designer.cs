1a333515bc491981f8a46366a1baf742d70551e2
ImfeldC <imfeldc@gmail.com>
2009-09-21 19:07:10 +0000

    Story #2: Add protocol configuration file to ClickOnce setup and change directory in the MSI setup project.

--- NuvoControl/Server/NuvoControl.Server.ProtocolDriver/Properties/Settings.Designer.cs
+++ NuvoControl/Server/NuvoControl.Server.ProtocolDriver/Properties/Settings.Designer.cs
@@ -52,7 +52,7 @@ namespace NuvoControl.Server.ProtocolDriver.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("./NuvoEssentiaProfile.xml")]
+        [global::System.Configuration.DefaultSettingValueAttribute("./Config/NuvoEssentiaProfile.xml")]
         public string NuvoEssentiaProfileFile {
             get {
                 return ((string)(this["NuvoEssentiaProfileFile"]));
