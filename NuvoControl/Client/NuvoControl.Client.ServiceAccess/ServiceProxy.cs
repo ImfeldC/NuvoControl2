5020885f75d6bf8945148d6225e0aa8be0c83d9b
ImfeldC <imfeldc@gmail.com>
2009-10-15 21:38:54 +0000

    Revert to revision Revision: 944 Author: ImfeldC Date: 20:21:09, Montag, 7. September 2009 Message: Adjust trace ouput, use mostly _log member to trace. This allows to re-direct the ouptut to debugview.

---
+++ NuvoControl/Client/NuvoControl.Client.ServiceAccess/ServiceProxy.cs
@@ -0,0 +1,120 @@
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Client.Viewer
+ *   Author:         Bernhard Limacher
+ *   Creation Date:  12.07.2009
+ *   File Name:      ServiceProxy.cs
+ * 
+ ***************************************************************************************************
+ * 
+ * 
+ **************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NuvoControl.Client.ServiceAccess.MonitorAndControlService;
+using NuvoControl.Client.ServiceAccess.ConfigurationService;
+
+namespace NuvoControl.Client.ServiceAccess
+{
+    /// <summary>
+    /// Static class, used to gain access from all over the viewer to the service proxies.
+    /// </summary>
+    public static class ServiceProxy
+    {
+        #region Fields
+
+        /// <summary>
+        /// M&C proxy
+        /// </summary>
+        public static MonitorAndControlProxy _monitorAndControlProxy = null;
+
+        /// <summary>
+        /// Configuration proxy
+        /// </summary>
+        public static ConfigurationProxy _configurationProxy = null;
+
+        /// <summary>
+        /// The ip or name of the client.
+        /// </summary>
+        public static string _clientIpOrName = "localhost";
+
+        #endregion
+
+        #region Public Interface
+
+
+        /// <summary>
+        /// Returns the M&C proxy.
+        /// </summary>
+        public static MonitorAndControlProxy MonitorAndControlProxy
+        {
+            get
+            {
+                if (_monitorAndControlProxy == null)
+                    _monitorAndControlProxy = new MonitorAndControlProxy(_clientIpOrName);
+                
+                return _monitorAndControlProxy;
+            }
+        }
+
+
+        /// <summary>
+        /// Returns the configuration proxy.
+        /// </summary>
+        public static ConfigurationProxy ConfigurationProxy
+        {
+            get
+            {
+                if (_configurationProxy == null)
+                    _configurationProxy = new ConfigurationProxy();
+
+                return _configurationProxy;
+            }
+        }
+
+
+        /// <summary>
+        /// Injects a configuration service proxy
+        /// </summary>
+        /// <param name="configurationProxy"></param>
+        public static void Inject(IConfigure configurationProxy)
+        {
+            _configurationProxy = new ConfigurationProxy(configurationProxy);
+        }
+
+
+        /// <summary>
+        /// Injects a M&C service proxy
+        /// </summary>
+        public static void Inject(IMonitorAndControl mAndCProxy)
+        {
+            _monitorAndControlProxy = new MonitorAndControlProxy(mAndCProxy);
+        }
+
+
+        /// <summary>
+        /// Sets the ip or the name of the client.
+        /// </summary>
+        public static string ClientIpOrName
+        {
+            set { _clientIpOrName = value; }
+        }
+
+        #endregion
+    }
+}
+
+/**************************************************************************************************
+ * 
+ *   Copyright (C) B. Limacher, C. Imfeld. All Rights Reserved. Confidential
+ * 
+**************************************************************************************************/
