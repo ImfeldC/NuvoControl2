daa863937f349c3dacd1ebb2f32a87b956720c89
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2010-08-15 16:07:17 +0000

    #21 Add two new helper classes: 1) NetworkHelper to help to retrieve information about the network, like IP address 2) AppInfoHelper to help to retrieve information about the running application.

---
+++ source/trunk/NuvoControl/Common/NuvoControl.Common/AppInfoHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuvoControl.Common
+{
+    public class AppInfoHelper
+    {
+        /// <summary>
+        /// Returns the assembly version.
+        /// </summary>
+        /// <returns></returns>
+        static public string getAssemblyVersion()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        /// <summary>
+        /// Returns the deployment (publish) version, if available; otherwise "(n/a)" is returned.
+        /// </summary>
+        /// <returns></returns>
+        static public string getDeploymentVersion()
+        {
+            string deploymentVersion="";
+            try
+            {
+                deploymentVersion = System.Deployment.Application.ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
+            }
+            catch( Exception )
+            {
+                deploymentVersion = "(n/a)";
+            }
+            return deploymentVersion;
+        }
+    }
+}
