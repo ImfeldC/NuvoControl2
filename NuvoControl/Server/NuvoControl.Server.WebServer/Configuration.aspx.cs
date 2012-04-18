5e99eee00184099c8a5090e923521800aef1d8fc
ImfeldC <imfeldc@gmail.com>
2012-04-18 21:22:48 +0000

    - Add logger functionality. Combine trace and ASP.NET logging. - Add Service Manager, which reads out the configuration. - Enable ASP.NEt logging/tracing

--- NuvoControl/Server/NuvoControl.Server.WebServer/Configuration.aspx.cs
+++ NuvoControl/Server/NuvoControl.Server.WebServer/Configuration.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Common.Logging;
+using Common.Logging.Simple;
+
 using NuvoControl.Common.Configuration;
 using NuvoControl.Common;
 
@@ -15,25 +18,29 @@ namespace NuvoControl.Server.WebServer
     public partial class WebForm1 : System.Web.UI.Page
     {
 
-        protected void LoadConfiguration()
+        private static ILog _log = LogManager.GetCurrentClassLogger();
+
+        protected void GetConfiguration()
         {
+            _log.Trace(m => m("LoadConfiguration started."));
+
             labelConfiguration.Text = "";
 
             ConfigureClient cfgIfc = null;
             cfgIfc = new ConfigureClient();
             // Connect to the discovered service endpoint
-            cfgIfc.Endpoint.Address = Global.DiscoveredConfigurationClients.Endpoints[0].Address;
+            cfgIfc.Endpoint.Address = Global.ServiceManager.DiscoveredConfigurationClients.Endpoints[0].Address;
 
-            for( int iZoneId=1; iZoneId<12; iZoneId++ )
+            for (int iZoneId = 1; iZoneId < 12; iZoneId++)
             {
-                Console.WriteLine("Read zone configuration for zone with id {0}.", iZoneId);
+                _log.Trace(m => m("Read zone configuration for zone with id {0}.", iZoneId));
                 Zone zone = cfgIfc.GetZoneKonfiguration(new Address(100, iZoneId));
                 labelConfiguration.Text += zone.ToString();
                 labelConfiguration.Text += "\n";
             }
 
             Graphic graphic = cfgIfc.GetGraphicConfiguration();
-            Console.WriteLine("All graphic details: {0}", graphic.ToString());
+            _log.Trace(m => m("All graphic details: {0}", graphic.ToString()));
 
             labelConfiguration.Text += graphic.ToString();
         }
@@ -41,7 +48,7 @@ namespace NuvoControl.Server.WebServer
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            LoadConfiguration();
+            GetConfiguration();
         }
     }
 }
