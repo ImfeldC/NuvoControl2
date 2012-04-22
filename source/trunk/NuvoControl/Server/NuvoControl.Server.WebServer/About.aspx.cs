d78aec245d71d784ef88abc7b68755658b227bfc
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2012-04-22 08:00:57 +0000

    - Add further information to the About page

--- source/trunk/NuvoControl/Server/NuvoControl.Server.WebServer/About.aspx.cs
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.WebServer/About.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using NuvoControl.Common;
 
 namespace NuvoControl.Server.WebServer
 {
@@ -11,7 +12,16 @@ namespace NuvoControl.Server.WebServer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // Add product information
+            labelAppInfo.Text = "<b>Application Information:</b> <br>";
+            labelAppInfo.Text += String.Format("Assembly Version = {0}<br>", AppInfoHelper.getAssemblyVersion());
+            labelAppInfo.Text += String.Format("Deployment Version = {0}<br>", AppInfoHelper.getDeploymentVersion());    
+        
+            // Add assembly information
+            labelAppInfo.Text += "<b>Assembly Information:</b> <br>";
+            labelAppInfo.Text += String.Format("Full Name = {0}<br>", System.Reflection.Assembly.GetExecutingAssembly().FullName);
+            labelAppInfo.Text += String.Format("Image Runtime Version = {0}<br>", System.Reflection.Assembly.GetExecutingAssembly().ImageRuntimeVersion);
+            labelAppInfo.Text += String.Format("Location = {0}<br>", System.Reflection.Assembly.GetExecutingAssembly().Location);
         }
     }
 }
