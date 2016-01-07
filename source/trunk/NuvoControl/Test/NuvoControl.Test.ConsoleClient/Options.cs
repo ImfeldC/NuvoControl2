97cfbe3ba81bdb9c8d12d93b03df5789000a2fd7
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2016-01-07 17:55:00 +0000

    Added a new test client. A simple console application, which allows to communicate with the Nuvo Essentia system. First tests on the Raspberry Pi worked fine: (a) Build application on PC - (b) Transfer binaries to Raspberry Pi - (c) Start executable - (d) It works :-)

---
+++ source/trunk/NuvoControl/Test/NuvoControl.Test.ConsoleClient/Options.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CommandLine;
+using CommandLine.Text; // if you want text formatting helpers (recommended)
+
+namespace NuvoControl.Test.ConsoleClient
+{
+    class Options
+    {
+      [Option('p', "portname", Required = true, HelpText = "Portname to connect. E.g. COM1 or /dev/ttyAMA0")]
+      public string portName { get; set; }
+
+      [Option('b', "baudrate", HelpText = "Baudrate used to connect. E.g. 9600")]
+      public int baudRate { get; set; }
+
+      [Option('r', "readtimeout", HelpText = "Read timeout [ms]. E.g. 4000")]
+      public int readTimeout { get; set; }
+
+      [Option('s', "senddata", HelpText = "Data to be send. E.g. *VER or *Z01OFF")]
+      public string sendData { get; set; }
+
+      [Option('v', null, HelpText = "Print details during execution.")]
+      public bool Verbose { get; set; }
+
+      [Option('?', "help", HelpText = "Print detailed help instructions.")]
+      public bool Help { get; set; }
+
+      [HelpOption]
+      public string GetUsage()
+      {
+        // this without using CommandLine.Text
+        //var usage = new StringBuilder();
+        //usage.AppendLine("Quickstart Application 1.0");
+        //usage.AppendLine("Read user manual for usage instructions...");
+        //return usage.ToString();
+
+        return HelpText.AutoBuild(this,
+            (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+      }
+    }
+}
---
+++ source/trunk/NuvoControl/Test/NuvoControl.Test.ConsoleClient/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.IO.Ports;
+
+namespace NuvoControl.Test.ConsoleClient
+{
+    class SerialPortTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> Starting Console Client  --- Assembly Version={0} / Deployment Version={1} / Product Version={2} (using .NET 4.0) ... ",
+                "n/a", "n/a", Application.ProductVersion);
+            //Console.WriteLine(">>> Starting Console Client  --- Assembly Version={0} / Deployment Version={1} / Product Version={2} (using .NET 4.0) ... ",
+            //    AppInfoHelper.getAssemblyVersion(), AppInfoHelper.getDeploymentVersion(), Application.ProductVersion);
+            Console.WriteLine();
+
+            var options = new Options();
+            CommandLine.Parser.Default.ParseArguments(args, options);
+            if (options.Help)
+            {
+                Console.WriteLine(options.GetUsage());
+            }
+
+            // Connect and Test
+            SerialPortTest myTest = new SerialPortTest(options.portName,options.baudRate,options.readTimeout);
+            myTest.Test();
+
+            if( options.sendData != null )
+                myTest.SendReceiveData(options.sendData + "\r\n");
+
+            Console.WriteLine(">>> Press <Enter> to stop the console application.");
+            Console.ReadLine();
+        }
+
+        private SerialPort mySerial;
+        private string _portName = "/dev/ttyAMA0";  // default port name within Raspberry Pi
+        private int _baudRate = 9600;               // default: 9600 baud
+        private int _readTimeout = 4000;            // default: 4000 [ms]
+ 
+
+        // Constructor
+        public SerialPortTest(string portName, int baudRate, int readTimeout)
+        {
+            _portName = portName;
+            if (baudRate > 0 )
+                _baudRate = baudRate;
+            if (readTimeout > 0)
+                _readTimeout = readTimeout;
+        }
+ 
+        public void Test()
+        {
+            // Test connection, with firmware query command
+            SendReceiveData("*VER\r\n");
+        }
+
+        public void SendReceiveData(string sendData)
+        {
+            if (mySerial != null)
+                if (mySerial.IsOpen)
+                    mySerial.Close();
+
+            try
+            {
+                mySerial = new SerialPort(_portName, _baudRate);
+                mySerial.Open();
+                mySerial.ReadTimeout = _readTimeout;
+                SendData(sendData);
+            }
+            catch (System.ArgumentException exc)
+            {
+                Console.WriteLine("Exception! {0}", exc.ToString());
+                mySerial = null;
+            }
+
+            if (mySerial != null)
+            {
+                string inputData = ReadData();
+                //Console.WriteLine("Message received:" + inputData);
+            }
+            Console.WriteLine("End ....");
+        }
+
+        public string ReadData()
+        {
+            byte tmpByte;
+            string rxString = "";
+ 
+            try
+            {
+                tmpByte = (byte)mySerial.ReadByte();
+                //Console.WriteLine("Start ...." + tmpByte);
+
+                while (tmpByte != 255)
+                {
+                    rxString += ((char)tmpByte);
+                    tmpByte = (byte)mySerial.ReadByte();
+                    //Console.WriteLine("Get data ...." + tmpByte);
+                }
+            }
+            catch (System.TimeoutException exc)
+            {
+                // ignore timeout, finish read-out
+            }
+
+            Console.WriteLine("Message received:" + rxString);
+            return rxString;
+        }
+ 
+        public bool SendData(string Data)
+        {
+            mySerial.Write(Data);
+            Console.WriteLine("Message send:" + Data);
+            return true;
+        }
+
+    }
+}
