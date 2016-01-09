7ffbf47931f11fc69f1aabd821a3a3aeabe6e5b0
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2016-01-09 16:56:30 +0000

    Create base class for common command line options, to be used/supported by several tools

---
+++ source/trunk/NuvoControl/Common/NuvoControl.Common/CommonOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// see https://commandline.codeplex.com/
+using CommandLine;
+using CommandLine.Text; // if you want text formatting helpers (recommended)
+
+
+namespace NuvoControl.Common
+{
+    public class CommonOptions
+    {
+        [Option('v', "verbose", HelpText = "Enable verbose mode. Print-out more messages.")]
+        public int volume { get; set; }
+
+        [Option('?', "help", HelpText = "Print detailed help instructions.")]
+        public bool Help { get; set; }
+
+        [HelpOption(HelpText = "Dispaly this help screen.")]
+        public string GetUsage()
+        {
+            // this without using CommandLine.Text
+            //var usage = new StringBuilder();
+            //usage.AppendLine("Quickstart Application 1.0");
+            //usage.AppendLine("Read user manual for usage instructions...");
+            //return usage.ToString();
+
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
