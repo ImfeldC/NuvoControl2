7a48f290a862e5d03b48f6de4ad0f47a687f4643
ImfeldC <imfeldc@gmail.com>
2016-01-09 09:38:56 +0000

    First simple command line server.

---
+++ NuvoControl/Server/NuvoControl.Server.HostConsole/HostConsole.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;     // used for Application
+using System.Text;
+//using System.IO.Ports;
+
+using Common.Logging;
+
+using NuvoControl.Common;
+using NuvoControl.Common.Configuration;
+
+using NuvoControl.Server.ProtocolDriver;
+using NuvoControl.Server.ProtocolDriver.Interface;
+
+using NuvoControl.Server.ConfigurationService;
+using NuvoControl.Server.FunctionServer;
+using NuvoControl.Server.ZoneServer;
+
+
+
+namespace NuvoControl.Server.HostConsole
+{
+    class HostConsole
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetCurrentClassLogger();
+            log.Debug(m => m("Starting Server Console! (Version={0})", Application.ProductVersion));
+            LogHelper.Log("**** Server Console started. *******", log);
+
+            Console.WriteLine(">>> Starting Server Console  --- Assembly Version={0} / Deployment Version={1} / Product Version={2} (using .NET 4.0) ... ",
+                "n/a", "n/a", Application.ProductVersion);
+            //Console.WriteLine(">>> Starting Server Console  --- Assembly Version={0} / Deployment Version={1} / Product Version={2} (using .NET 4.0) ... ",
+            //    AppInfoHelper.getAssemblyVersion(), AppInfoHelper.getDeploymentVersion(), Application.ProductVersion);
+            Console.WriteLine("    Linux={0} / Detected environment: {1}", EnvironmentHelper.isRunningOnLinux(), EnvironmentHelper.getOperatingSystem());
+            Console.WriteLine();
+
+            // Load command line argumnets
+            var options = new Options();
+            CommandLine.Parser.Default.ParseArguments(args, options);
+            if (options.Help)
+            {
+                Console.WriteLine(options.GetUsage());
+            }
+
+            LoadAllServices();
+
+            Console.WriteLine(">>> ");
+            Console.WriteLine(">>> ");
+            Console.WriteLine(">>> Press <Enter> to stop the console application.");
+            Console.ReadLine();
+
+            UnloadAllServices();
+
+        }
+
+        #region Fields
+
+        private static ILog _log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Holds the loaded system configuration.
+        /// </summary>
+        private static NuvoControl.Server.ConfigurationService.ConfigurationService _configurationService = null;
+
+        /// <summary>
+        /// Holds the protocol drivers per NuvoEssentia.
+        /// </summary>
+        private static Dictionary<int, IProtocol> _protocolDrivers = new Dictionary<int, IProtocol>();
+
+        /// <summary>
+        /// Holds a reference to the zone server.
+        /// </summary>
+        private static IZoneServer _zoneServer = null;
+
+        /// <summary>
+        /// Holds a reference to the function server.
+        /// </summary>
+        private static FunctionServer.FunctionServer _functionServer = null;
+
+        /// <summary>
+        /// Private member to hold the timer used to periodically check for configuration changes
+        /// </summary>
+        private static System.Timers.Timer _timerCheckConfiguration = new System.Timers.Timer();
+
+        #endregion
+
+
+        private static void LoadAllServices()
+        {
+            try
+            {
+                //LoadConfigurationService(Properties.Settings.Default.NuvoControlKonfigurationFile);
+                LoadConfigurationService(Properties.Settings.Default.NuvoControlKonfigurationFile, Properties.Settings.Default.NuvoControlRemoteKonfigurationFile);
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to load the system configuration.", exc);
+                Console.WriteLine("Failed to load the system configuration. Exception message: {0}", exc.Message);
+                return;
+            }
+
+            try
+            {
+                LoadProtocolDrivers();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to load the protocol drivers.", exc);
+                Console.WriteLine("Failed to load the protocol drivers. Exception message: {0}", exc.Message);
+            }
+
+            try
+            {
+                InstantiateZoneServer();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to create the zone server.", exc);
+                Console.WriteLine("Failed to create the zone server. Exception message: {0}", exc.Message);
+            }
+
+            try
+            {
+                InstantiateFunctionServer();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to create the function server.", exc);
+                Console.WriteLine("Failed to create the function server. Exception message: {0}", exc.Message);
+            }
+
+            Console.WriteLine();
+        }
+
+
+        private static void UnloadAllServices()
+        {
+            try
+            {
+                UnloadFunctionServer();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to unload the function server.", exc);
+                Console.WriteLine("Failed to unload the function server. Exception message: {0}", exc.Message);
+            }
+
+            try
+            {
+                UnloadZoneServer();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to unload the zone server.", exc);
+                Console.WriteLine("Failed to unload the zone server. Exception message: {0}", exc.Message);
+            }
+
+            try
+            {
+                UnloadProtocolDrivers();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to unload the protocol drivers.", exc);
+                Console.WriteLine("Failed to unload the protocol drivers. Exception message: {0}", exc.Message);
+            }
+
+            try
+            {
+                UnloadConfigurationService();
+            }
+            catch (Exception exc)
+            {
+                _log.ErrorFormat("Failed to load the system configuration.", exc);
+                Console.WriteLine("Failed to load the system configuration. Exception message: {0}", exc.Message);
+                return;
+            }
+
+            Console.WriteLine();
+        }
+
+
+        /// <summary>
+        /// Reads the XML configuration file and instantiates accordingly the system configuration objects of NuvoControl.
+        /// </summary>
+        /// <param name="configurationFile">Filename of configuration file</param>
+        /// <param name="remoteConfigurationFile">Filename of configuration file to append</param>
+        private static void LoadConfigurationService(string configurationFile, string remoteConfigurationFile)
+        {
+            _log.Info(m => m("Loading the nuvo control configuration from '{0}' and '{1}' ...", configurationFile, remoteConfigurationFile));
+            Console.WriteLine(">>> Loading configuration...");
+            Console.WriteLine(">>>   from {0}", configurationFile);
+            Console.WriteLine(">>>   and append {0}", remoteConfigurationFile);
+
+            _configurationService = new NuvoControl.Server.ConfigurationService.ConfigurationService(configurationFile, remoteConfigurationFile);
+        }
+
+        /// <summary>
+        /// Unloads the system configuration objects of NuvoControl.
+        /// </summary>
+        /// <param name="configurationFile">Filename of configuration file</param>
+        private static void UnloadConfigurationService()
+        {
+            _log.Info(m => m("Unload the nuvo control configuration ..."));
+            Console.WriteLine(">>> Unload the nuvo control configuration ...");
+
+            _configurationService.Dispose();
+            _configurationService = null;
+        }
+
+
+        /// <summary>
+        /// Loads the protocol drivers according to the NuvoControl system configuration.
+        /// </summary>
+        private static void LoadProtocolDrivers()
+        {
+            _log.Info("Loading protocol drivers...");
+            Console.WriteLine(">>> Loading protocol drivers...");
+
+            foreach (Device device in _configurationService.SystemConfiguration.Hardware.Devices)
+            {
+                IProtocol driver = ProtocolDriverFactory.LoadDriver(device.ProtocolDriver.AssemblyName, device.ProtocolDriver.ClassName);
+                if (driver != null)
+                {
+                    driver.Open(ENuvoSystem.NuVoEssentia, device.Id, device.Communication);
+                    _protocolDrivers[device.Id] = driver;
+                    Console.WriteLine(">>>   driver {0} loaded ...", device.Id);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Unload the protocol drivers.
+        /// </summary>
+        private static void UnloadProtocolDrivers()
+        {
+            _log.Info("Unload protocol drivers...");
+            Console.WriteLine(">>> Unload protocol drivers...");
+
+            foreach (int deviceId in _protocolDrivers.Keys)
+            {
+                _protocolDrivers[deviceId].Close(deviceId);
+                //_protocolDrivers[deviceId] = null;
+                Console.WriteLine(">>>   driver {0} unloaded ...", deviceId);
+            }
+            _protocolDrivers.Clear();
+        }
+
+        /// <summary>
+        /// Instantiates the zone server. This object holds all zone controllers.
+        /// </summary>
+        private static void InstantiateZoneServer()
+        {
+            _log.Info("Instantiating the zone server...");
+            Console.WriteLine(">>> Instantiating the zone server...");
+
+            List<IZoneController> zoneControllers = new List<IZoneController>();
+            foreach (Device device in _configurationService.SystemConfiguration.Hardware.Devices)
+            {
+                foreach (int zoneId in device.Zones)
+                {
+                    zoneControllers.Add(new ZoneController(new Address(device.Id, zoneId), _protocolDrivers[device.Id]));
+                }
+            }
+            _zoneServer = new NuvoControl.Server.ZoneServer.ZoneServer(zoneControllers);
+            _zoneServer.StartUp();
+        }
+
+        /// <summary>
+        /// Unload the zone server. This object holds all zone controllers.
+        /// </summary>
+        private static void UnloadZoneServer()
+        {
+            _log.Info("Unload the zone server...");
+            Console.WriteLine(">>> Unload the zone server...");
+
+            _zoneServer.ShutDown();
+            _zoneServer = null;
+        }
+
+
+        /// <summary>
+        /// Instantiates the function server. This object holds all functions.
+        /// </summary>
+        private static void InstantiateFunctionServer()
+        {
+            _log.Info("Instantiating the function server...");
+            Console.WriteLine(">>> Instantiating the function server...");
+
+            _functionServer = new NuvoControl.Server.FunctionServer.FunctionServer(_zoneServer, _configurationService.SystemConfiguration.Functions);
+            //_functionServer.StartUp();
+        }
+
+        /// <summary>
+        /// Unload the function server. This object holds all functions.
+        /// </summary>
+        private static void UnloadFunctionServer()
+        {
+            _log.Info("Unload the function server...");
+            Console.WriteLine(">>> Unload the function server...");
+
+            _functionServer = null;
+        }
+
+        
+
+    }
+}
