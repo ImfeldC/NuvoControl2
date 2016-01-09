6237d4e5c17b0c39c846ce0a486ad2a8fb2770f6
ImfeldC <ImfeldC@cc0574c9-ca68-0410-b7a3-859615599fd7>
2016-01-09 23:03:52 +0000

    Add CommandLine library, which has been built by myself Move common command line options into a common class, to be reused by other applications.

--- source/trunk/NuvoControl/Server/NuvoControl.Server.HostConsole/Options.cs
+++ source/trunk/NuvoControl/Server/NuvoControl.Server.HostConsole/Options.cs
@@ -6,9 +6,12 @@ using System.Text;
 using CommandLine;
 using CommandLine.Text; // if you want text formatting helpers (recommended)
 
+using NuvoControl.Common;       // used for CommonOptions
+
+
 namespace NuvoControl.Server.HostConsole
 {
-    class Options
+    class Options : CommonOptions
     {
       [Option('p', "portname", Required = true, HelpText = "Portname to connect. E.g. COM1 or /dev/ttyAMA0")]
       public string portName { get; set; }
@@ -19,38 +22,23 @@ namespace NuvoControl.Server.HostConsole
       [Option('r', "readtimeout", HelpText = "Read timeout [ms]. E.g. 4000")]
       public int readTimeout { get; set; }
 
-      [Option('s', "senddata", HelpText = "Data to be send. E.g. *VER or *Z01OFF")]
+      [Option("senddata", HelpText = "Data to be send. E.g. *VER or *Z01OFF")]
       public string sendData { get; set; }
 
-      [Option('c', "command", HelpText = "Nuvo Essentia command to be send. E.g. ReadVersion or TurnALLZoneOFF (see ENuvoEssentiaCommands)")]
+      [Option("command", HelpText = "Nuvo Essentia command to be send. E.g. ReadVersion or TurnALLZoneOFF (see ENuvoEssentiaCommands)")]
       public string strCommand { get; set; }
 
-      [Option('z', "zone", HelpText = "Nuvo Essentia zone identifier. E.g. Zone1 (see ENuvoEssentiaZones)")]
+      [Option("zone", HelpText = "Nuvo Essentia zone identifier. E.g. Zone1 (see ENuvoEssentiaZones)")]
       public string strZone { get; set; }
 
-      [Option('o', "source", HelpText = "Nuvo Essentia source identifier. E.g. Source1 (see ENuvoEssentiaSources)")]
+      [Option("source", HelpText = "Nuvo Essentia source identifier. E.g. Source1 (see ENuvoEssentiaSources)")]
       public string strSource { get; set; }
 
-      [Option('w', "powerstatus", HelpText = "Nuvo Essentia zone power status. Either ZoneStatusOFF or ZoneStatusON (see EZonePowerStatus)")]
+      [Option("powerstatus", HelpText = "Nuvo Essentia zone power status. Either ZoneStatusOFF or ZoneStatusON (see EZonePowerStatus)")]
       public string strPowerStatus { get; set; }
 
-      [Option('v', "volume", HelpText = "Volume level.")]
+      [Option("volume", HelpText = "Volume level.")]
       public int volume { get; set; }
 
-      [Option('?', "help", HelpText = "Print detailed help instructions.")]
-      public bool Help { get; set; }
-
-      [HelpOption]
-      public string GetUsage()
-      {
-        // this without using CommandLine.Text
-        //var usage = new StringBuilder();
-        //usage.AppendLine("Quickstart Application 1.0");
-        //usage.AppendLine("Read user manual for usage instructions...");
-        //return usage.ToString();
-
-        return HelpText.AutoBuild(this,
-            (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
-      }
     }
 }
