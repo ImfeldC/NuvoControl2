26906ab18e16472782d65e3a0ca56de16fe00b6f
ImfeldC <imfeldc@gmail.com>
2016-01-14 22:04:37 +0000

    - Add Mail helper Class - Add base command Class Diagram/Configuration.cd(not finished yet)

---
+++ NuvoControl/Common/NuvoControl.Common/Configuration/Command.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace NuvoControl.Common.Configuration
+{
+    [DataContract]
+    public class Command
+    {
+        /// <summary>
+        /// The id of the command.
+        /// </summary>
+        //[DataMember]
+        //private Guid _id = Guid.NewGuid();
+
+        [DataMember]
+        private string _command;
+
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public Command()
+        {
+        }
+
+        public Command(string command)
+        {
+            _command = command;
+        }
+
+        /// <summary>
+        /// Accessor for the function id.
+        /// </summary>
+        //public Guid Id
+        //{
+        //    get { return _id; }
+        //}
+
+        /// <summary>
+        /// Public override of ToString() method.
+        /// </summary>
+        /// <returns>Returns string representative.</returns>
+        public override string ToString()
+        {
+            return String.Format("Command={0}", _command);
+        }
+
+    }
+}
---
+++ NuvoControl/Common/NuvoControl.Common/MailHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Mail;
+
+
+namespace NuvoControl.Common
+{
+    // Example from http://stackoverflow.com/questions/9201239/send-e-mail-via-smtp-using-c-sharp
+    // Setup on Raspberry Pi, see http://stackoverflow.com/questions/4148019/authentication-or-decryption-has-failed-when-sending-mail-to-gmail-using-ssl
+    //   mozroots --import --ask-remove --machine
+    //   certmgr -ssl smtps://smtp.gmail.com:465
+    public class MailHelper
+    {
+        private const string your_id = "imfeldc@gmail.com";
+        private const string your_password = "DIGITAL";
+
+        /// <summary>
+        /// Sends mail message
+        /// </summary>
+        /// <param name="recepient">Recepient mail address.</param>
+        /// <param name="subject">Mail subject.</param>
+        /// <param name="body">Mail body.</param>
+        /// <returns>true in case send was succesful, otherwise false.</returns>
+        public static bool SendMail(string recepient, string subject, string body)
+        {
+            return SendMail(recepient, subject, body, your_id, your_password);
+        }
+
+        /// <summary>
+        /// Sends mail message
+        /// </summary>
+        /// <param name="recepient">Recepient mail address.</param>
+        /// <param name="subject">Mail subject.</param>
+        /// <param name="body">Mail body.</param>
+        /// <param name="myid">GMail username.</param>
+        /// <param name="mypassword">GMail password.</param>
+        /// <returns>true in case send was succesful, otherwise false.</returns>
+        public static bool SendMail(string recepient, string subject, string body, string myid, string mypassword)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new System.Net.NetworkCredential(myid, mypassword),
+                    Timeout = 10000,
+                };
+                MailMessage mm = new MailMessage(your_id, recepient, subject, body);
+                client.Send(mm);
+
+                //Console.WriteLine("Email Sent");
+                return true;
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Could not send email\n\n" + e.ToString());
+                return false;
+            }
+        }
+
+    }
+}
