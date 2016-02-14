a4c8eb83ff98d31d4561224607c9d9bdd4fa319e
ImfeldC <imfeldc@gmail.com>
2016-02-14 13:44:31 +0000

    - Add renew play sound command every 60[min]

--- NuvoControl/Server/NuvoControl.Server.FunctionServer/Command/ConcretePlaySoundCommand.cs
+++ NuvoControl/Server/NuvoControl.Server.FunctionServer/Command/ConcretePlaySoundCommand.cs
@@ -15,8 +15,18 @@ namespace NuvoControl.Server.FunctionServer
 {
     class ConcretePlaySoundCommand : ConcreteCommand, IDisposable
     {
+        #region Private Fields
+
+        private bool _isSoundPlaying = false;
         private PlaySoundCommand _playSoundCommand = null;
         private Process _process = null;
+
+        /// <summary>
+        /// Private member to hold the timer used to periodically renew the play sound command
+        /// </summary>
+        private System.Timers.Timer _timerRenewPlaySoundCommand = new System.Timers.Timer();
+
+        #endregion
 
         public ConcretePlaySoundCommand(PlaySoundCommand command)
             :base( command )
@@ -24,6 +34,11 @@ namespace NuvoControl.Server.FunctionServer
             _playSoundCommand = command;
         }
 
+        /// <summary>
+        /// Method called for execCommand
+        /// </summary>
+        /// <param name="cmdType">Command type.</param>
+        /// <param name="function">Related fucntion.</param>
         public override void execCommand(eCommandType cmdType, Function function)
         {
             // onFunctionStart & onValidityStart configured to run ...
@@ -32,14 +47,7 @@ namespace NuvoControl.Server.FunctionServer
                 LogHelper.Log(LogLevel.Info, String.Format(">>> Execute PlaySound@START command on event {0}", cmdType));
                 LogHelper.Log(LogLevel.Trace, String.Format("      PlaySoundCommand={0} / Function={1}", _playSoundCommand.ToString(), function.ToString()));
 
-                if (EnvironmentHelper.isRunningOnLinux())
-                {
-                    playSoundOnUnix();
-                }
-                else
-                {
-                    playSound();
-                }
+                playSound();
             }
 
             // onFunctionEnd & onValidityEnd configured to run ...
@@ -55,14 +63,35 @@ namespace NuvoControl.Server.FunctionServer
             // ... stop any running process in case of an error
             if (checkCommandType(cmdType) && cmdType == eCommandType.onFunctionError)
             {
+                LogHelper.Log(LogLevel.Info, String.Format(">>> Execute PlaySound@ERROR command on event {0}", cmdType));
+                LogHelper.Log(LogLevel.Trace, String.Format("      PlaySoundCommand={0} / Function={1}", _playSoundCommand.ToString(), function.ToString()));
+
                 killProcess();
+            }
+        }
+
+
+        #region Play Sound
+
+        /// <summary>
+        /// Play sound, calls OS related play sound method.
+        /// </summary>
+        private void playSound()
+        {
+            if (EnvironmentHelper.isRunningOnLinux())
+            {
+                playSoundOnUnix();
+            }
+            else
+            {
+                playSoundOnWindows();
             }
         }
 
         /// <summary>
         /// Play sound on Windows platform
         /// </summary>
-        private void playSound()
+        private void playSoundOnWindows()
         {
             // Example URI:
             // - http://www.tonycuffe.com/mp3/tail%20toddle.mp3
@@ -72,18 +101,24 @@ namespace NuvoControl.Server.FunctionServer
             // - http://asx.skypro.ch/radio/internet-128/virus.asx
 
             LogHelper.Log(LogLevel.All, "   Beep!");
-            LogHelper.Log(LogLevel.Info, String.Format("    ... play sound on Windows! Process={0}", (_process != null ? _process.ToString() : "(null)")));
+            _isSoundPlaying = true;
+            LogHelper.Log(LogLevel.Info, String.Format("Play sound on Windows! Process={0}", (_process != null ? _process.ToString() : "(null)")));
         }
 
         /// <summary>
-        /// Play sound on Unix platform
+        /// Play sound on Unix platform. Stops (kills) any running play sound command.
         /// </summary>
         private void playSoundOnUnix()
         {
             killProcess();
             _process = EnvironmentHelper.run_cmd("/usr/bin/mpg321", _playSoundCommand.Url);
-            LogHelper.Log(LogLevel.Info, String.Format("    ... play sound on Unix! Process={0}", (_process != null ? _process.ToString() : "(null)") ));
+            _isSoundPlaying = true;
+            LogHelper.Log(LogLevel.Info, String.Format("Play sound on Unix! Process={0}", (_process != null ? _process.ToString() : "(null)") ));
         }
+
+        #endregion
+
+        #region Kill Process
 
         /// <summary>
         /// Kill process
@@ -104,10 +139,43 @@ namespace NuvoControl.Server.FunctionServer
                     //ignore any exception at shutdown
                     LogHelper.LogException("Cannot kill process in 'killProcess' method!", exc);
                 }
+                _isSoundPlaying = false;
+                LogHelper.Log(LogLevel.Info, String.Format("Play sound stopped! Process={0}", (_process != null ? _process.ToString() : "(null)")));
             }
         }
 
-        public void Dispose()
+        #endregion
+
+        #region Renew Play Sound Command
+
+        private void StartTime()
+        {
+            LogHelper.Log(LogLevel.Debug, String.Format("Renew play sound command, each {0}[min]", Properties.Settings.Default.RenewPlaySoundCommandIntervall*60));
+            _timerRenewPlaySoundCommand.Interval = (Properties.Settings.Default.RenewPlaySoundCommandIntervall < 10 ? 10 : Properties.Settings.Default.RenewPlaySoundCommandIntervall) * 1000 * 60;
+            _timerRenewPlaySoundCommand.Elapsed += new System.Timers.ElapsedEventHandler(_timerRenewPlaySoundCommand_Elapsed);
+            _timerRenewPlaySoundCommand.Start();
+        }
+
+        /// <summary>
+        /// Periodic timer routine to renew play sound command.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _timerRenewPlaySoundCommand_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (_isSoundPlaying)
+            {
+                LogHelper.Log(LogLevel.Info, String.Format(">>> Renew play sound command"));
+                playSound();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Dispose method, to esnure that play sound command (process) gets killed at shutdown
+        /// </summary>
+        public new void Dispose()
         {
             // kill any process, before shutdown
             killProcess();
