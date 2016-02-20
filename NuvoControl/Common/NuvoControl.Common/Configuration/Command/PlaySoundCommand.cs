447cf8e23058b25405a01ed46be9119238a94f84
ImfeldC <imfeldc@gmail.com>
2016-02-20 15:32:17 +0000

    - move graphic configuration classes into an own subfolder - Add AudioDevice class (as part of Device) - Add SourceId to the play sound class

--- NuvoControl/Common/NuvoControl.Common/Configuration/Command/PlaySoundCommand.cs
+++ NuvoControl/Common/NuvoControl.Common/Configuration/Command/PlaySoundCommand.cs
@@ -1,23 +1,46 @@
-﻿using System;
+﻿/**************************************************************************************************
+ * 
+ *   Copyright (C) 2016 by Ch. Imfeld. All Rights Reserved.
+ * 
+ ***************************************************************************************************
+ *
+ *   Project:        NuvoControl
+ *   SubProject:     NuvoControl.Common
+ * 
+ **************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace NuvoControl.Common.Configuration
 {
+    /// <summary>
+    /// This is a system configuration class. 
+    /// 
+    /// It is a data structurer.
+    /// It conatins the defintion of the play sound command.
+    /// </summary>
     public class PlaySoundCommand : Command
     {
 
-        private string _url = "";
+        #region Private Members
 
         /// <summary>
-        /// URL to play.
+        /// The address of the source.
         /// </summary>
-        public string Url
-        {
-            get { return _url; }
-            set { _url = value; }
-        }
+        private Address _sourceId = new Address(SystemConfiguration.ID_UNDEFINED, SystemConfiguration.ID_UNDEFINED);
+
+        /// <summary>
+        /// The URL to play.
+        /// </summary>
+        private string _url = "";
+
+        #endregion
+
+
+        #region Constructors
 
         /// <summary>
         /// Standard constructor, used by configuration loader.
@@ -30,12 +53,39 @@ namespace NuvoControl.Common.Configuration
         /// <param name="onValidityEnd">True, if command shall be executed at validity end.</param>
         /// <param name="onUnix">True, if command shall be exceuted on Unix systems. Default=True</param>
         /// <param name="onWindows">True, if command shall be executed on Windows systems. Default=True</param>
+        /// <param name="sourceId">Source Id, on which to play the sound.</param>
+        /// <param name="url">URL to play.</param>
         public PlaySoundCommand(Guid id, bool onFunctionError, bool onFunctionStart, bool onFunctionEnd, bool onValidityStart, bool onValidityEnd, bool onUnix, bool onWindows,
-            string url )
-            :base(id, eCommand.PlaySound, onFunctionError, onFunctionStart, onFunctionEnd, onValidityStart, onValidityEnd, onUnix, onWindows)
+            Address sourceId, string url)
+            : base(id, eCommand.PlaySound, onFunctionError, onFunctionStart, onFunctionEnd, onValidityStart, onValidityEnd, onUnix, onWindows)
         {
+            _sourceId = sourceId;
             _url = url;
         }
+
+        #endregion
+
+
+        #region Public Interface
+
+        /// <summary>
+        /// Source id where to play the sound.
+        /// </summary>
+        public Address SourceId
+        {
+            get { return _sourceId; }
+            set { _sourceId = value; }
+        }
+
+        /// <summary>
+        /// URL to play.
+        /// </summary>
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+
 
         /// <summary>
         /// Public override of ToString() method.
@@ -46,6 +96,8 @@ namespace NuvoControl.Common.Configuration
             return String.Format("PlaySound=[ url={1}, Command={0} ]",
                 base.ToString(), _url);
         }
+
+        #endregion
 
     }
 }
