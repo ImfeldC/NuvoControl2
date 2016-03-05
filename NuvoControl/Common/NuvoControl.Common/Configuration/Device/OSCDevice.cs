7ad6bc28ba81e51791593b75938215906a2aa003
ImfeldC <imfeldc@gmail.com>
2016-03-05 15:22:35 +0000

    - Add OSC device settings, read from XML configuration file

---
+++ NuvoControl/Common/NuvoControl.Common/Configuration/Device/OSCDevice.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NuvoControl.Common.Configuration
+{
+
+    public enum eOSCDeviceType
+    {
+        OSCServer = 0,
+        OSCClient = 1
+    }
+
+    
+    /// <summary>
+    /// This is a system configuration class. 
+    /// 
+    /// It is a data structurer.
+    /// It defines a OSC device.
+    /// </summary>
+    [DataContract]
+    public class OSCDevice
+    {
+
+        #region Private Members
+
+        private eOSCDeviceType _deviceType = eOSCDeviceType.OSCServer;
+
+        private Address _deviceId = new Address();
+
+        private string _name = "";
+
+        private IPAddress _ipAddress = null;
+
+        private int _port = 0;
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public OSCDevice()
+        {
+        }
+
+        /// <summary>
+        /// Standard Constructor
+        /// </summary>
+        public OSCDevice(eOSCDeviceType deviceType, Address deviceId, string name, IPAddress ipAddress, int port)
+        {
+            _deviceType = deviceType;
+            _deviceId = deviceId;
+            _name = name;
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        #endregion
+
+
+        #region Public Interface
+
+        public eOSCDeviceType DeviceType
+        {
+            get { return _deviceType; }
+        }
+
+        public Address DeviceId
+        {
+            get { return _deviceId; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public IPAddress IpAddress
+        {
+            get { return _ipAddress; }
+        }
+
+        public int Port
+        {
+            get { return _port; }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Returns string representative of this class.
+        /// </summary>
+        /// <returns>String representative of this class.</returns>
+        public override string ToString()
+        {
+            return String.Format("Name={0}, Id={1}, Type={2}, IPAddress={3}, Port={4}", _name, _deviceId.ToString(), _deviceType.ToString(), _ipAddress.ToString(), _port);
+        }
+
+    }
+}
